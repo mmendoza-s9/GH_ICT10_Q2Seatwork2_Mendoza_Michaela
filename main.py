65dd0fabc2fff098f849f80ce9ec519ab2b82d50
mixs <mendoza.mc@obmontessori.edu.ph>
2025-11-17 12:11:23 +0800

    main.py

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ number_of_units = 5, 5, 3, 1, 1, 1
 total_units = 16
 
 def student_gwa(e):
-    document.getElementById("output").innerHTML = " "
+    document.getElementById("output").innerHTML = " " 
 
-    eng_grade = float(document.getElementById('eng').value)
+    eng_grade = float(document.getElementById('english').value)
     eng_result = eng_grade * 5
 
     math_grade = float(document.getElementById('math').value)
@@ -27,5 +27,10 @@ def student_gwa(e):
     ve_grade = float(document.getElementById('ve').value)
     ve_result = ve_grade * 1
 
-    sum = ((eng_result + math_result + ss_result + music_result + pe_result + ve_result) / total_units)
+    sum = ((eng_result + math_result + ss_result + music_result + pe_result + ve_result) // total_units)
+
+    first_name = document.getElementById('fname').value
+    last_name = document.getElementById('lname').value
+
+    display(f'Name: {first_name} {last_name}', target='output')
     display(f'Your GWA is: {sum}', target='output')
